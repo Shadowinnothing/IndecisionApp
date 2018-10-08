39a5f900cf740df6b902c152222a12c58522d204
Shadowinnothing <shadowinnothing@gmail.com>
2018-10-08 17:49:50 -0600

    Added normalize.css to make app look the same in all browsers

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Container to run app
 import IndecisionApp from './components/IndecisionApp';
 
-import './styles/styles.scss';
+// normalize is used to break default values down so app
+// looks the same in all browsers
+import 'normalize.css/normalize.css';
+import './styles/styles.scss'; // <- custom scss files
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
